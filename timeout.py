937475251914ae4f890c4a96b0ef467cbfe21a04
Jeremy Koritzinsky <jkoritzinsky@gmail.com>
2017-10-06 14:08:44 -0500

    Implement timeout learning algorithm

--- timeout.py
+++ timeout.py
@@ -1,7 +1,23 @@
+import datetime
+
+class Address():
+    def __init__(self, addr, port, timestamp):
+        self.addr = addr
+        self.port = port
+        self.timestamp = timestamp
+
 addrs = []
 
 def learnAddr(src, input_port):
-    return
+    global addrs
+    now = datetime.datetime.now()
+    addrs = [addr for addr in addrs
+        if (now - addr.timestamp).total_seconds() < 10]
+    addrs = addrs + [Address(src, input_port, now)]
 
 def getAddr(dest):
+    global addrs
+    record = next((addr for addr in addrs if addr.addr == dest), None)
+    if record != None:
+        return record.port
     return None
